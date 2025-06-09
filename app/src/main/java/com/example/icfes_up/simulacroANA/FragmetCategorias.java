3629ec17882304a7a1351cb49be374b08611c6ad
Maria2007 <anamariazuniga202@gmail.com>
2025-06-09 14:18:16 -0500

    simulacros

---
+++ app/src/main/java/com/example/icfes_up/simulacroANA/FragmetCategorias.java
@@ -0,0 +1,54 @@
+package com.example.icfes_up.simulacroANA;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
+import com.example.icfes_up.databinding.FragmentCategoriasBinding;
+
+public class FragmentCategorias extends Fragment {
+    private FragmentCategoriasBinding binding;
+
+    public FragmentCategorias() {}
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        binding = FragmentCategoriasBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        binding.itemLectura.setOnClickListener(v ->
+                Navigation.findNavController(v).navigate(R.id.action_fragmentCategorias_to_simulacroLectura1)
+        );
+
+        binding.itemMatematicas.setOnClickListener(v ->
+                Navigation.findNavController(v).navigate(R.id.action_fragmentCategorias_to_simulacroMatematicas1)
+        );
+
+        binding.itemSociales.setOnClickListener(v ->
+                Navigation.findNavController(v).navigate(R.id.action_fragmentCategorias_to_simulacroSociales1)
+        );
+
+        binding.itemCiencias.setOnClickListener(v ->
+                Navigation.findNavController(v).navigate(R.id.action_fragmentCategorias_to_simulacroNaturales1)
+        );
+
+        binding.itemIngles.setOnClickListener(v ->
+                Navigation.findNavController(v).navigate(R.id.action_fragmentCategorias_to_simulacroIngles1)
+        );
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
