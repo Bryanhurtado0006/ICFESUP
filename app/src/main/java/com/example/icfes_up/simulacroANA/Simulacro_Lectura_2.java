df6f59de02e7b8d1cbb88db1b6208b9076d89e65
Maria2007 <anamariazuniga202@gmail.com>
2025-06-09 20:40:26 -0500

    simulacros y estadistica

--- app/src/main/java/com/example/icfes_up/simulacroANA/Simulacro_Lectura_2.java
+++ app/src/main/java/com/example/icfes_up/simulacroANA/Simulacro_Lectura_2.java
@@ -1,66 +1,40 @@
 package com.example.icfes_up.simulacroANA;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
+import com.example.icfes_up.databinding.FragmentSimulacroLectura2Binding;
 
 import com.example.icfes_up.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link Simulacro_Lectura_2#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class Simulacro_Lectura_2 extends Fragment {
+    private FragmentSimulacroLectura2Binding binding;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    public Simulacro_Lectura_2() {}
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public Simulacro_Lectura_2() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Simulacro_Lectura_2.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static Simulacro_Lectura_2 newInstance(String param1, String param2) {
-        Simulacro_Lectura_2 fragment = new Simulacro_Lectura_2();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        binding = FragmentSimulacroLectura2Binding.inflate(inflater, container, false);
+        return binding.getRoot();
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        binding.btnSiguiente2.setOnClickListener(v -> {
+            Navigation.findNavController(v).navigate(R.id.action_lectura_Simulacro_2_to_lectura_Simulacro_3);
+        });
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_simulacro_lectura_2, container, false);
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
     }
 }
