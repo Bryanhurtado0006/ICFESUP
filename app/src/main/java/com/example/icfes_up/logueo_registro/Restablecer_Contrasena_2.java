f947807d41847e6d8e77b845a653c1549a7ce5df
Daniel Diaz <carlosdanielpinzadiaz@gmail.com>
2025-06-16 15:26:16 -0500

    login

---
+++ app/src/main/java/com/example/icfes_up/logueo_registro/Restablecer_Contrasena_2.java
@@ -0,0 +1,32 @@
+package com.example.icfes_up.logueo_registro;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.icfes_up.databinding.ActivityRestablecerContrasena2Binding;
+
+public class Restablecer_Contrasena_2 extends AppCompatActivity {
+
+    private ActivityRestablecerContrasena2Binding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+
+        binding = ActivityRestablecerContrasena2Binding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+
+        binding.btnContinuar.setOnClickListener(v -> {
+            Intent intent = new Intent(Restablecer_Contrasena_2.this, Restablecer_Contrasena_3.class);
+            startActivity(intent);
+        });
+    }
+}
